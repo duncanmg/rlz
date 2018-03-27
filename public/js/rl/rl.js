58761ea3c8c7ae733fda1714bb6bd2062a36d8f1
Duncan Garland <duncan.garland@ntlworld.com>
2018-03-27 12:52:44 +0100

    Minor bits.

--- public/js/rl/rl.js
+++ public/js/rl/rl.js
@@ -1,14 +1,14 @@
 // rl/question/index.phtml
- function checkForm()
-  {
-      document.getElementsByName('answer')[0].value = 'Do not know';
-      return true;
-  }
+function checkForm()
+{
+    document.getElementsByName('answer')[0].value = 'Do not know';
+    return true;
+}
 
 // rl/charmap/charmap.phtml
 $(".charmap_button_div").find(".btn").click(function () {
-        var a = $("[name=answer]");
-        a.val(a.val()+ this.innerHTML);
-        a.focus();
-        return false;
+    var a = $("[name=answer]");
+    a.val(a.val() + this.innerHTML);
+    a.focus();
+    return false;
 });
